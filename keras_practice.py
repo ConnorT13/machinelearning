1c317b3843314a1a76fa9539be713aef6e83308b
ConnorT13 <connor.tan@gmail.com>
2024-07-01 15:15:29 -0400

    working on keras

--- keras_practice.py
+++ keras_practice.py
@@ -1,8 +1,18 @@
 import numpy as np
 from tensorflow import keras
+import mnist
 
-# Load the dataset (downloads automatically the first time)
-(train_images, train_labels), _ = keras.datasets.mnist.load_data()
+train_images = mnist.train_images()
+train_labels = mnist.train_labels()
+test_images = mnist.test_images()
+test_labels = mnist.test_labels()
 
-print(train_images.shape) # (60000, 28, 28)
-print(train_labels.shape) # (60000,)
+train_images = (train_images / 255) - 0.5
+test_labels = (test_images / 255) - 0.5
+
+# flattens images by taking the 28 * 28 2-D array, and transforms it into a 1-D Array
+train_images = train_images.reshape((-1, 784))
+test_images = test_images.reshape((-1,784))
+
+print(train_images.shape)
+print(test_images.shape)
